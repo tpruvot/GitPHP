420a5b8422dbf7464cbc1930aae1258c816b10b8
Chris Han <xiphux@gmail.com>
2012-06-09 00:48:04 -0500

    Forgot the debug header on the end benchmark line

--- include/Log.class.php
+++ include/Log.class.php
@@ -244,7 +244,7 @@ class GitPHP_Log
 			}
 
 			if ($this->benchmark) {
-				$data[] = '[' . $endTime . '] [' . ($endTime - $this->startTime) . ' sec since start] [' . ($endTime - $lastTime) . ' sec since last] [' . $endMem . ' bytes] [' . ($endMem - $this->startMem) . ' bytes since start] [' . ($endMem - $lastMem) . ' bytes since last] End';
+				$data[] = 'DEBUG: [' . $endTime . '] [' . ($endTime - $this->startTime) . ' sec since start] [' . ($endTime - $lastTime) . ' sec since last] [' . $endMem . ' bytes] [' . ($endMem - $this->startMem) . ' bytes since start] [' . ($endMem - $lastMem) . ' bytes since last] End';
 			}
 		}
 
