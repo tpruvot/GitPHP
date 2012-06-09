6a96149c3844c124910177062235533732cca1e9
Chris Han <xiphux@gmail.com>
2012-06-09 12:50:13 -0500

    Support on-demand projectlist loading for legacy array format

--- include/git/ProjectListArrayLegacy.class.php
+++ include/git/ProjectListArrayLegacy.class.php
@@ -55,22 +55,53 @@ class GitPHP_ProjectListArrayLegacy extends GitPHP_ProjectListBase
 	 */
 	protected function PopulateProjects()
 	{
-		$projectRoot = GitPHP_Util::AddSlash(GitPHP_Config::GetInstance()->GetValue('projectroot'));
-
 		foreach ($this->projectConfig as $cat => $plist) {
 			if (is_array($plist)) {
 				foreach ($plist as $pname => $ppath) {
 					try {
-						$projObj = new GitPHP_Project($projectRoot, $ppath);
-						if ($cat != GITPHP_NO_CATEGORY)
-							$projObj->SetCategory($cat);
-						$this->projects[$ppath] = $projObj;
+						$projObj = $this->InstantiateProject($ppath);
+						if ($projObj) {
+							$this->projects[$ppath] = $projObj;
+							unset($projObj);
+						}
 					} catch (Exception $e) {
 						GitPHP_Log::GetInstance()->Log($e->getMessage());
 					}
 				}
 			}
 		}
+	}
+
+	/**
+	 * InstantiateProject
+	 *
+	 * Instantiates project object
+	 *
+	 * @access protected
+	 * @param string $proj project
+	 * @return mixed project
+	 */
+	protected function InstantiateProject($proj)
+	{
+		$found = false;
+		$projectCat = GITPHP_NO_CATEGORY;
+		foreach ($this->projectConfig as $cat => $plist) {
+			if (is_array($plist) && (array_search($proj, $plist) !== false)) {
+				$found = true;
+				$projectCat = $cat;
+				break;
+			}
+		}
+
+		if (!$found) {
+			return;
+		}
+
+		$projectObj = new GitPHP_Project($this->projectRoot, $proj);
+		if ($projectCat != GITPHP_NO_CATEGORY)
+			$projectObj->SetCategory($projectCat);
+
+		return $projectObj;
 	}
 
 }
