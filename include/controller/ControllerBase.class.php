524bacb9c947216ceac589a0fec41b4b24d88e6a
Chris Han <xiphux@gmail.com>
2012-06-09 21:15:18 -0500

    Disable trimwhitespace
    trimwhitespace uses pcre to trim whitespace, but on large pages
    it hits the pcre.backtrack_limit and crashes out, leaving you with
    a blank page

--- include/controller/ControllerBase.class.php
+++ include/controller/ControllerBase.class.php
@@ -338,7 +338,7 @@ abstract class GitPHP_ControllerBase
 		}
 
 		if (!$this->preserveWhitespace) {
-			$this->tpl->loadFilter('output', 'trimwhitespace');
+			//$this->tpl->loadFilter('output', 'trimwhitespace');
 		}
 
 		$this->tpl->display($this->GetTemplate(), $this->GetFullCacheKey());
