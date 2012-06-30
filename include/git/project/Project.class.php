9b16f6e0f704757498c93f9853be9f40b7313f6f
Chris Han <xiphux@gmail.com>
2012-06-29 22:40:50 -0500

    Avoid infinite loop when expanding hash

--- include/git/project/Project.class.php
+++ include/git/project/Project.class.php
@@ -718,7 +718,9 @@ class GitPHP_Project
 		}
 
 		if (preg_match('/^[0-9A-Fa-f]{4,39}$/', $hash)) {
-			return $this->GetCommit($this->ExpandHash($hash));
+			$fullHash = $this->ExpandHash($hash);
+			if ($fullHash != $hash)
+				return $this->GetCommit($fullHash);
 		}
 
 		return null;
