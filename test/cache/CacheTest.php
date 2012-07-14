0f9ca05eedc7785f805876e906e40daf54fe61ef
Chris Han <xiphux@gmail.com>
2012-07-13 19:24:22 -0500

    Implement memcache and memcached for object cache

--- test/cache/CacheTest.php
+++ test/cache/CacheTest.php
@@ -48,21 +48,91 @@ class GitPHP_CacheTest extends PHPUnit_Framework_TestCase
 
 	public function testMemcache()
 	{
-		$this->markTestIncomplete();
+		if (!class_exists('Memcache')) {
+			$this->markTestSkipped();
+			return;
+		}
+		$cache = new GitPHP_Cache(new GitPHP_Cache_Memcache(array(array('127.0.0.1', 11211))));
+		$cache->Clear();
+
+		$this->assertFalse($cache->Exists('testkey1|testkey2'));
+		$cache->Set('testkey1|testkey2', 'testvalue1');
+		$this->assertTrue($cache->Exists('testkey1|testkey2'));
+		$this->assertEquals('testvalue1', $cache->Get('testkey1|testkey2'));
+
+		$this->assertFalse($cache->Get('testkey3|testkey4'));
+		$cache->Set('testkey3|testkey4', 'testvalue2');
+
+		$cache->Delete('testkey1|testkey2');
+		$this->assertFalse($cache->Exists('testkey1|testkey2'));
+
+		$this->assertTrue($cache->Exists('testkey3|testkey4'));
+
+		$cache->Clear();
+		$this->assertFalse($cache->Exists('testkey3|testkey4'));
 	}
 
 	public function testMemcacheLifetime()
 	{
-		$this->markTestIncomplete();
+		if (!class_exists('Memcache')) {
+			$this->markTestSkipped();
+			return;
+		}
+		$cache = new GitPHP_Cache(new GitPHP_Cache_Memcache(array(array('127.0.0.1', 11211))));
+		$cache->Clear();
+
+		$cache->Set('testkey1|testkey2', 'testvalue1', 1);
+		sleep(2);
+		$this->assertFalse($cache->Exists('testkey1|testkey2'));
+
+		$cache->SetLifetime(1);
+		$cache->Set('testkey3|testkey4', 'testvalue2');
+		sleep(2);
+		$this->assertFalse($cache->Get('testkey3|testkey4'));
 	}
 
 	public function testMemcached()
 	{
-		$this->markTestIncomplete();
+		if (!class_exists('Memcached')) {
+			$this->markTestSkipped();
+			return;
+		}
+		$cache = new GitPHP_Cache(new GitPHP_Cache_Memcached(array(array('127.0.0.1', 11211))));
+		$cache->Clear();
+
+		$this->assertFalse($cache->Exists('testkey1|testkey2'));
+		$cache->Set('testkey1|testkey2', 'testvalue1');
+		$this->assertTrue($cache->Exists('testkey1|testkey2'));
+		$this->assertEquals('testvalue1', $cache->Get('testkey1|testkey2'));
+
+		$this->assertFalse($cache->Get('testkey3|testkey4'));
+		$cache->Set('testkey3|testkey4', 'testvalue2');
+
+		$cache->Delete('testkey1|testkey2');
+		$this->assertFalse($cache->Exists('testkey1|testkey2'));
+
+		$this->assertTrue($cache->Exists('testkey3|testkey4'));
+
+		$cache->Clear();
+		$this->assertFalse($cache->Exists('testkey3|testkey4'));
 	}
 
 	public function testMemcachedLifetime()
 	{
-		$this->markTestIncomplete();
+		if (!class_exists('Memcached')) {
+			$this->markTestSkipped();
+			return;
+		}
+		$cache = new GitPHP_Cache(new GitPHP_Cache_Memcached(array(array('127.0.0.1', 11211))));
+		$cache->Clear();
+
+		$cache->Set('testkey1|testkey2', 'testvalue1', 1);
+		sleep(2);
+		$this->assertFalse($cache->Exists('testkey1|testkey2'));
+
+		$cache->SetLifetime(1);
+		$cache->Set('testkey3|testkey4', 'testvalue2');
+		sleep(2);
+		$this->assertFalse($cache->Get('testkey3|testkey4'));
 	}
 }
