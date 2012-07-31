5528c382819283d4313874396bf69a539e655b3c
Chris Han <xiphux@gmail.com>
2012-07-30 21:12:09 -0500

    Fix head commit url

--- include/Router.class.php
+++ include/Router.class.php
@@ -284,11 +284,12 @@ class GitPHP_Router
 
 		$params = array();
 
-		if (preg_match('@^projects/([^/\?]+)/commits(/[0-9A-Fa-f]{4,40}|HEAD)?$@', $url, $regs)) {
+		if (preg_match('@^projects/([^/\?]+)/commits(/([0-9A-Fa-f]{4,40}|HEAD))?$@', $url, $regs)) {
 			$params['p'] = rawurldecode($regs[1]);
 			$params['a'] = 'commit';
-			if (!empty($regs[2]))
-				$params['h'] = ltrim($regs[2], "/");
+			if (!empty($regs[3])) {
+				$params['h'] = $regs[3];
+			}
 		}
 
 		if (preg_match('@^projects/([^/\?]+)/trees(/[0-9A-Fa-f]{4,40})?$@', $url, $regs)) {
