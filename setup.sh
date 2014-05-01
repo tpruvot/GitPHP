ea429ca0f8357f8c50dc662eb80f52e115d36e3f
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2014-05-01 20:19:20 +0200

    setup: fix config test

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ chgrp -R www-data *
 chmod -R g+w templates_c
 chmod -R g+w cache
 
-if [ ! -x "config/gitphp.conf.php" ]; then
+if [ ! -f "config/gitphp.conf.php" ]; then
 	cp config/gitphp.conf.php.example config/gitphp.conf.php
 fi
 
-if [ ! -x "config/projects.conf.php" ]; then
+if [ ! -f "config/projects.conf.php" ]; then
 	cp config/projects.conf.php.example config/projects.conf.php
 fi
 
